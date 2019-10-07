9163028789f32f2c3e5cb41344375abef65d07fb
Tim Slifer <tim@slifer.dev>
2019-10-07 18:00:35 -0400

    Added BrowserCommands

---
+++ src/main/java/io/slifer/automation/core/BrowserCommands.java
@@ -0,0 +1,73 @@
+package io.slifer.automation.core;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Commands used to interact with a web browser.
+ *
+ * @author Tim Slifer
+ */
+public class BrowserCommands extends WebDriverCommands {
+    
+    public BrowserCommands(WebDriver webDriver) {
+        super(webDriver);
+    }
+    
+    /**
+     * Accepts a JavaScript Alert.
+     */
+    public void acceptAlert() {
+        webDriver.switchTo().alert().accept();
+    }
+    
+    /**
+     * Dismisses a JavaScript Alert.
+     */
+    public void dismissAlert() {
+        webDriver.switchTo().alert().dismiss();
+    }
+    
+    /**
+     * Closes the current browser window.
+     */
+    public void closeBrowser() {
+        webDriver.close();
+    }
+    
+    /**
+     * Navigates back to the previous page.
+     */
+    public void goBack() {
+        webDriver.navigate().back();
+    }
+    
+    /**
+     * Navigates forward to the next page, if one exists.
+     */
+    public void goForward() {
+        webDriver.navigate().forward();
+    }
+    
+    /**
+     * Refreshes the current page.
+     */
+    public void refreshPage() {
+        webDriver.navigate().refresh();
+    }
+    
+    /**
+     * Deletes all current cookies.
+     */
+    public void deleteCookies() {
+        webDriver.manage().deleteAllCookies();
+    }
+    
+    /**
+     * Navigates to the given URL.
+     *
+     * @param url The URL to be opened.
+     */
+    public void openUrl(String url) {
+        webDriver.get(url);
+    }
+}
