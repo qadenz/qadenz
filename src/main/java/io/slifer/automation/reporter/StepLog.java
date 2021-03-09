486d1f9b4d088a373b1674312ba1db3365589374
Tim Slifer <tim@slifer.io>
2021-03-08 22:00:38 -0500

    Adding test log processing steps and object tree for json export

---
+++ src/main/java/io/slifer/automation/reporter/StepLog.java
@@ -0,0 +1,32 @@
+package io.slifer.automation.reporter;
+
+public class StepLog {
+    
+    public String timestamp;
+    public String level;
+    public String message;
+    
+    public String getTimestamp() {
+        return timestamp;
+    }
+    
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+    
+    public String getLevel() {
+        return level;
+    }
+    
+    public void setLevel(String level) {
+        this.level = level;
+    }
+    
+    public String getMessage() {
+        return message;
+    }
+    
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
