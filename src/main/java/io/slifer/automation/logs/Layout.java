cfbffb7626e9bde56db65ef2737e78c7a1530855
Tim Slifer <tim-slifer@users.noreply.github.com>
2021-03-15 23:35:58 -0400

    Refinement to layout

--- src/main/java/io/slifer/automation/logs/Layout.java
+++ src/main/java/io/slifer/automation/logs/Layout.java
@@ -3,7 +3,6 @@ package io.slifer.automation.logs;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.CoreConstants;
 import ch.qos.logback.core.LayoutBase;
-import io.slifer.automation.config.RunContext;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -18,13 +17,12 @@ public class Layout extends LayoutBase<ILoggingEvent> {
     @Override
     public String doLayout(ILoggingEvent event) {
         StringBuffer buffer = new StringBuffer();
+        
         buffer.append(new SimpleDateFormat("HH:mm:ss:SSS").format(new Date()));
         buffer.append("|");
-        buffer.append(RunContext.getTestCaseName());
+        buffer.append(event.getLoggerName().substring(event.getLoggerName().lastIndexOf(".") + 1));
         buffer.append("|");
         buffer.append(event.getLevel());
-        buffer.append("|");
-        buffer.append(event.getLoggerName().substring(event.getLoggerName().lastIndexOf(".") + 1));
         buffer.append("|");
         buffer.append(event.getFormattedMessage());
         buffer.append(CoreConstants.LINE_SEPARATOR);
