2b047305a5df654f6d33ae8eecf65a073277f9e3
Tim Slifer <tim@slifer.io>
2021-03-28 22:25:47 -0400

    Initial resizing and base64 encoding of screenshots added

--- src/main/java/io/slifer/automation/reporter/Screenshots.java
+++ src/main/java/io/slifer/automation/reporter/Screenshots.java
@@ -4,13 +4,18 @@ import io.slifer.automation.config.RunContext;
 import org.imgscalr.Scalr;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.slf4j.Logger;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.util.Base64;
 import java.util.HashMap;
 
 public class Screenshots extends HashMap<String, String> {
+    
+    public static final Logger LOG = RunContext.SUITE_LOG;
     
     public void captureScreenshot() {
         File rawCapture = ((TakesScreenshot) RunContext.getWebDriver()).getScreenshotAs(OutputType.FILE);
@@ -23,20 +28,38 @@ public class Screenshots extends HashMap<String, String> {
     }
     
     private BufferedImage resize(File rawCapture) {
+        LOG.debug("Resizing screen capture.");
         try {
             BufferedImage original = ImageIO.read(rawCapture);
             int originalWidth = original.getWidth();
+            LOG.debug("Detected original capture width [{}].", originalWidth);
             int targetWidth = originalWidth / 2;
             
-            Scalr.resize(original, targetWidth);
+            BufferedImage resized = Scalr.resize(original, targetWidth);
+            LOG.debug("Resized to width [{}].", resized.getWidth());
+            
+            return resized;
         }
         catch (Exception e) {
-            e.printStackTrace();
+            LOG.error("Error resizing image.", e);
+            
+            throw new RuntimeException(e);
         }
-        return null;
     }
     
     private String convertToBase64(BufferedImage resizedCapture) {
-        return "";
+        LOG.debug("Converting to Base64.");
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            ImageIO.write(resizedCapture, "png", outputStream);
+            byte[] bytes = outputStream.toByteArray();
+            
+            return Base64.getEncoder().encodeToString(bytes);
+        }
+        catch (Exception e) {
+            LOG.error("Error converting image.", e);
+            
+            throw new RuntimeException(e);
+        }
     }
 }
