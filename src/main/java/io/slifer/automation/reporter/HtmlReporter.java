d7d544db3e7c1b3d66b693652cfcefa2b882a31d
Tim Slifer <tim@slifer.dev>
2021-03-31 01:25:26 -0400

    HTML reporter getting started
    Writing the Head, processing css file, writing to html file

--- src/main/java/io/slifer/automation/reporter/HtmlReporter.java
+++ src/main/java/io/slifer/automation/reporter/HtmlReporter.java
@@ -1,5 +1,15 @@
 package io.slifer.automation.reporter;
 
+import org.apache.commons.io.FileUtils;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class HtmlReporter {
     
     private JsonReport json;
@@ -9,6 +19,49 @@ public class HtmlReporter {
     }
     
     public void generateReport() {
-        // first, build the suite config
+        Document document = Document.createShell("");
+        writeHead(document);
+        writeSummary(document);
+        
+        writeHtmlFile(document);
+    }
+    
+    private void writeHead(Document document) {
+        Element head = document.head();
+        head.appendElement("meta").attr("charset", "UTF-8");
+        head.appendElement("title").text("Test Report");
+        head.appendElement("link")
+            .attr("href", "https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@600&display=swap")
+            .attr("rel", "stylesheet");
+        head.appendElement("style").text(loadAndMinifyCss());
+    }
+    
+    private String loadAndMinifyCss() {
+        
+        String contents = null;
+        try {
+            Path path = Paths.get(ClassLoader.getSystemResource("html/report.css").toURI());
+            
+            contents = Files.readString(path, StandardCharsets.UTF_8);
+        }
+        catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        
+        return contents.replaceAll("\\s", "");
+    }
+    
+    private void writeSummary(Document document) {
+        
+    }
+    
+    private void writeHtmlFile(Document document) {
+        try {
+            File file = new File("Automation-Report.html");
+            FileUtils.writeStringToFile(file, document.outerHtml(), StandardCharsets.UTF_8);
+        }
+        catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
