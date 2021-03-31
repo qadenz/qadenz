0cb25084177f1a566c4ee7ceb78b64f02511e0ad
Tim Slifer <tim@slifer.io>
2021-03-31 00:24:37 -0400

    Basic HTML reporter call added

---
+++ src/main/java/io/slifer/automation/reporter/HtmlReporter.java
@@ -0,0 +1,14 @@
+package io.slifer.automation.reporter;
+
+public class HtmlReporter {
+    
+    private JsonReport json;
+    
+    public HtmlReporter(JsonReport json) {
+        this.json = json;
+    }
+    
+    public void generateReport() {
+        // first, build the suite config
+    }
+}
