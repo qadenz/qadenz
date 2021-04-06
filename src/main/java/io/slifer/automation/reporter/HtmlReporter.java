a6dbb58cef01af7ee60f97d33fd8ee0801f974f4
Tim Slifer <tim-slifer@users.noreply.github.com>
2021-04-06 00:08:06 -0400

    Added writing results section block with stubbed data, no loops yet

--- src/main/java/io/slifer/automation/reporter/HtmlReporter.java
+++ src/main/java/io/slifer/automation/reporter/HtmlReporter.java
@@ -22,6 +22,11 @@ public class HtmlReporter {
         Document document = Document.createShell("");
         writeHead(document);
         writeSummary(document.body());
+        writeResultsSection(document.body(), HtmlResult.FAILED);
+        writeResultsSection(document.body(), HtmlResult.STOPPED);
+        writeResultsSection(document.body(), HtmlResult.SKIPPED);
+        writeResultsSection(document.body(), HtmlResult.PASSED);
+        writeScript(document.body());
         
         writeHtmlFile(document);
     }
@@ -75,6 +80,79 @@ public class HtmlReporter {
         totalTestsItem.appendElement("div").attr("class", "summary-item-value " + valueAttribute).text(value);
     }
     
+    private void writeResultsSection(Element body, HtmlResult result) {
+        body.appendElement("div").attr("class", "results-section bordered");
+        Element section = body.getElementsByClass("results-section bordered").last();
+        section.appendElement("div").attr("class", "section-name bordered " + result.resultsSectionStyle)
+               .text(result.resultsSectionLabel);
+        section.appendElement("div").attr("class", "test-classes bordered");
+        Element classes = section.getElementsByClass("test-classes bordered").last();
+        
+        writeTestClass(classes);
+        
+        body.appendElement("br");
+    }
+    
+    private void writeTestClass(Element classes) {
+        classes.appendElement("div").attr("class", "test-class accordion").text("io.slifer.test.cases.SampleTest");
+        classes.appendElement("div").attr("class", "test-methods panel hide");
+        Element methods = classes.getElementsByClass("test-methods panel hide").last();
+        
+        writeTestMethod(methods);
+    }
+    
+    private void writeTestMethod(Element methods) {
+        methods.appendElement("div").attr("class", "test-method");
+        Element testMethod = methods.getElementsByClass("test-method").last();
+        testMethod.appendElement("div").attr("class", "method-name accordion")
+                  .text("verifySomethingHappensWhenActionIsCompleted");
+        testMethod.appendElement("span").attr("class", "method-detail");
+        Element startTime = testMethod.getElementsByClass("method-detail").last();
+        startTime.appendElement("span").attr("class", "method-detail-label").text("Start Time");
+        startTime.appendElement("span").attr("class", "method-detail-value").text("99:99:99.999");
+        Element duration = testMethod.getElementsByClass("method-detail").last();
+        duration.appendElement("span").attr("class", "method-detail-label").text("Start Time");
+        duration.appendElement("span").attr("class", "method-detail-value").text("99:99:99.999");
+        testMethod.appendElement("div").attr("class", "method-logs");
+        Element methodLogs = testMethod.getElementsByClass("method-logs").last();
+        
+        writeMethodLogs(methodLogs);
+    }
+    
+    private void writeMethodLogs(Element methodLogs) {
+        methodLogs.appendElement("div").attr("class", "log-entry")
+                  .text("99:99:99.999 | INFO | This is a log entry for a test step.");
+    }
+    
+    private void writeScript(Element body) {
+        /*
+        "var acc = document.getElementsByClassName("accordion");"+
+        "var i;""+
+        "for (i = 0; i < acc.length; i++) {"+
+            "acc[i].addEventListener("click", function () {"+
+                "this.classList.toggle("active");"+
+                "var panel = this.nextElementSibling;"+
+                "if (panel.style.display === "block") {"+
+                "panel.style.display = "none";"+
+                "}"+
+                "else {"+
+                "panel.style.display = "block";"+
+                "}"+
+            "});"+
+        "}"
+         */
+        String script = "var acc = document.getElementsByClassName(\"accordion\");" +
+                "var i;" +
+                "for (i = 0; i < acc.length; i++) {" +
+                "acc[i].addEventListener(\"click\", function () {" +
+                "this.classList.toggle(\"active\");" +
+                "var panel = this.nextElementSibling;" +
+                "if (panel.style.display === \"block\") {" +
+                "panel.style.display = \"none\";}" +
+                "else {panel.style.display = \"block\";}});}";
+        body.appendElement("script").text(script);
+    }
+    
     private void writeHtmlFile(Document document) {
         try {
             File file = new File("Automation-Report.html");
@@ -83,5 +161,48 @@ public class HtmlReporter {
         catch (Exception exception) {
             exception.printStackTrace();
         }
+    }
+    
+    private enum HtmlResult {
+        FAILED("txt-failed", "Tests Failed", "bg-failed", "Failed Tests"),
+        PASSED("txt-passed", "Tests Passed", "bg-passed", "Passed Tests"),
+        SKIPPED("txt-skipped", "Tests Skipped", "bg-skipped", "Skipped Tests"),
+        STOPPED("txt-stopped", "Tests Stopped", "bg-stopped", "Stopped Tests");
+        
+        private String summaryItemValueStyle;
+        private String summaryItemLabel;
+        private String resultsSectionStyle;
+        private String resultsSectionLabel;
+        
+        HtmlResult(String summaryItemValueStyle, String summaryItemLabel, String resultsSectionStyle,
+                String resultsSectionLabel) {
+            this.summaryItemValueStyle = summaryItemValueStyle;
+            this.summaryItemLabel = summaryItemLabel;
+            this.resultsSectionStyle = resultsSectionStyle;
+            this.resultsSectionLabel = resultsSectionLabel;
+        }
+        
+        public String getSummaryItemValueStyle() {
+            return summaryItemValueStyle;
+        }
+        
+        public String getSummaryItemLabel() {
+            return summaryItemLabel;
+        }
+        
+        public String getResultsSectionStyle() {
+            return resultsSectionStyle;
+        }
+        
+        public String getResultsSectionLabel() {
+            return resultsSectionLabel;
+        }
+    }
+    
+    public static void main(String[] args) {
+        HtmlReporter r = new HtmlReporter(null);
+        r.generateReport();
+        
+        // System.out.println(r.loadAndMinifyCss());
     }
 }
