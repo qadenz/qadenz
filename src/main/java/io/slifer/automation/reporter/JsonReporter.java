591ebc9b035c1a509f627a07196618f280321276
Tim Slifer <tim-slifer@users.noreply.github.com>
2021-06-02 01:49:45 -0400

    JsonReporter in progress

--- src/main/java/io/slifer/automation/reporter/JsonReporter.java
+++ src/main/java/io/slifer/automation/reporter/JsonReporter.java
@@ -1,15 +1,30 @@
 package io.slifer.automation.reporter;
 
 import io.slifer.automation.config.RunContext;
+import io.slifer.automation.reporter.model.JsonClass;
+import io.slifer.automation.reporter.model.JsonMethod;
+import io.slifer.automation.reporter.model.JsonMethodLogEvent;
 import io.slifer.automation.reporter.model.JsonReport;
+import io.slifer.automation.reporter.model.JsonTest;
+import io.slifer.automation.reporter.testng.ClassResult;
+import io.slifer.automation.reporter.testng.MethodResult;
 import io.slifer.automation.reporter.testng.SuiteResult;
+import io.slifer.automation.reporter.testng.TestResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.ISuite;
+import org.testng.ITestResult;
+import org.testng.Reporter;
 import org.testng.xml.XmlSuite;
 
 import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class JsonReporter {
     
@@ -30,10 +45,13 @@ public class JsonReporter {
     }
     
     public void compileJsonReport() {
+        SuiteResult suiteResult = new SuiteResult(suite);
         
+        setSuiteHeaderDetails(suiteResult);
+        processSuiteResults(suiteResult);
     }
     
-    private void setSuiteHeaderDetails() {
+    private void setSuiteHeaderDetails(SuiteResult suiteResult) {
         jsonReport.setSuiteName(suite.getName());
         
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
@@ -52,5 +70,88 @@ public class JsonReporter {
         // jsonReport.setBrowserVersion(RunContext.browserVersion);
         // jsonReport.setPlatform(RunContext.platform.name()); 
         // jsonReport.setAppUrl(RunContext.appUrl);
+    }
+    
+    private void processSuiteResults(SuiteResult suiteResult) {
+        List<JsonTest> jsonTests = new ArrayList<>();
+        for (TestResult testResult : suiteResult.getTestResults()) {
+            jsonTests.add(processTestResults(testResult));
+        }
+        
+        jsonReport.setJsonTests(jsonTests);
+    }
+    
+    private JsonTest processTestResults(TestResult testResult) {
+        JsonTest jsonTest = new JsonTest();
+        jsonTest.setTestName(testResult.getTestName());
+        
+        List<JsonClass> failedConfigurations = processClassResults(testResult.getFailedConfigurationResults());
+        List<JsonClass> failedTests = processClassResults(testResult.getFailedTestResults());
+        List<JsonClass> stoppedTests = processClassResults(testResult.getStoppedTestResults());
+        List<JsonClass> skippedConfigurations = processClassResults(testResult.getSkippedConfigurationResults());
+        List<JsonClass> skippedTests = processClassResults(testResult.getSkippedTestResults());
+        List<JsonClass> passedTests = processClassResults(testResult.getPassedTestResults());
+        
+        jsonTest.setFailedConfigurations(failedConfigurations);
+        jsonTest.setFailedTests(failedTests);
+        jsonTest.setStoppedTests(stoppedTests);
+        jsonTest.setSkippedConfigurations(skippedConfigurations);
+        jsonTest.setSkippedTests(skippedTests);
+        jsonTest.setPassedTests(passedTests);
+        
+        return jsonTest;
+    }
+    
+    private List<JsonClass> processClassResults(List<ClassResult> classResults) {
+        List<JsonClass> jsonClasses = new ArrayList<>();
+        for (ClassResult classResult : classResults) {
+            JsonClass jsonClass = new JsonClass();
+            jsonClass.setClassName(classResult.getClassName());
+            jsonClass.setMethods(processMethodResults(classResult.getMethodResults()));
+            
+            jsonClasses.add(jsonClass);
+        }
+        
+        return jsonClasses;
+    }
+    
+    private List<JsonMethod> processMethodResults(List<MethodResult> methodResults) {
+        List<JsonMethod> jsonMethods = new ArrayList<>();
+        for (MethodResult methodResult : methodResults) {
+            JsonMethod jsonMethod = new JsonMethod();
+            ITestResult result = methodResult.getResults().iterator().next();
+            
+            jsonMethod.setMethodName(result.getName());
+            jsonMethod.setParameters(Arrays.toString(result.getParameters()));
+            
+            LocalDateTime startDateMillis =
+                    Instant.ofEpochMilli(result.getStartMillis()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss:SSS");
+            String testStartTime = startDateMillis.format(formatter);
+            jsonMethod.setTestStartTime(testStartTime);
+            
+            LocalDateTime endDateMillis =
+                    Instant.ofEpochMilli(result.getEndMillis()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+            Duration duration = Duration.between(startDateMillis, endDateMillis);
+            String testExecutionTime = String.format("%02dm %02d.%02ds",
+                    duration.toMinutesPart(), duration.toSecondsPart(), duration.toMillisPart());
+            jsonMethod.setTestExecutionTime(testExecutionTime);
+            
+            // handle screenshots
+            
+            // handle logging events
+            List<String> logOutput = Reporter.getOutput(result);
+            List<JsonMethodLogEvent> logEvents = processMethodLogOutput(logOutput);
+            jsonMethod.setLogEvents(logEvents);
+            
+            jsonMethods.add(jsonMethod);
+        }
+        
+        return jsonMethods;
+    }
+    
+    private List<JsonMethodLogEvent> processMethodLogOutput(List<String> logOutput) {
+        // TODO maybe we don't need to process these events? perhaps the string list is enough and we don't need 
+        //  separate fields on the json report. it ends up as a concatenated String anyway on html.
     }
 }
