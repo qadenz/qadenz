cd14bb83a364f7504a20100153e70f3764da555c
Tim Slifer <tim@slifer.io>
2021-06-02 17:53:29 -0400

    Writing json results file

--- src/main/java/io/slifer/automation/reporter/TestReporter.java
+++ src/main/java/io/slifer/automation/reporter/TestReporter.java
@@ -1,11 +1,16 @@
 package io.slifer.automation.reporter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import io.slifer.automation.config.RunContext;
+import io.slifer.automation.reporter.model.JsonReport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.IReporter;
 import org.testng.ISuite;
 import org.testng.xml.XmlSuite;
 
+import java.io.File;
 import java.util.List;
 
 public class TestReporter implements IReporter {
@@ -17,7 +22,15 @@ public class TestReporter implements IReporter {
         
         LOG.info("Starting JSON Compilation.");
         JsonReporter jsonReporter = new JsonReporter(xmlSuites.get(0), suites.get(0));
-        jsonReporter.compileJsonReport();
+        JsonReport jsonReport = jsonReporter.compileJsonReport();
+        
+        try {
+            ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+            objectWriter.writeValue(new File(RunContext.reportOutputPath + "results.json"), jsonReport);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
         
         // System.out.println("**********");
         // System.out.println("*");
