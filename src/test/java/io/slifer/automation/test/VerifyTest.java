20bd1702847b819a2f0c6f7eddcf20c82f0227bd
Tim Slifer <tim-slifer@users.noreply.github.com>
2021-07-08 18:42:26 -0400

    Added initial set of verify tests, in progress
    Now that assertions against expected exceptions are working, it's time to move onto asserting invocation counts

--- src/test/java/io/slifer/automation/test/VerifyTest.java
+++ src/test/java/io/slifer/automation/test/VerifyTest.java
@@ -1,28 +1,70 @@
 package io.slifer.automation.test;
 
-import io.slifer.automation.reporter.Screenshots;
+import io.slifer.automation.reporter.Screenshot;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.stubbing.Answer;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith (MockitoExtension.class)
 public class VerifyTest {
     
+    @Mock
+    private Screenshot screenshot;
+    
+    @InjectMocks
+    private TestCommander cmd;
+    
     @Test
-    public void verifyWillPassWithOneConditionThatReturnsTrue() {
-        TestCommander cmd = new TestCommander();
+    public void singleCondition_ReturnsTrue_ResultIsPass() {
         cmd.verify(TestConditions.returnsTrue());
     }
     
     @Test
-    public void verifyWillFailWithOneConditionThatReturnsFalse() {
-        try (MockedStatic<Screenshots> screenshots = Mockito.mockStatic(Screenshots.class)) {
-            TestCommander cmd = new TestCommander();
-            screenshots.when(Screenshots::captureScreen).thenAnswer((Answer<Void>) invocation -> null);
-            Assertions.assertThrows(AssertionError.class, () -> {
-                cmd.verify(TestConditions.returnsFalse());
-            });
-        }
+    public void singleCondition_ReturnsFalse_ResultIsFail() {
+        Mockito.doNothing().when(screenshot).capture();
+        Assertions.assertThrows(AssertionError.class, () -> cmd.verify(TestConditions.returnsFalse()));
+    }
+    
+    @Test
+    public void singleCondition_ThrowsError_ResultIsError() {
+        Mockito.doNothing().when(screenshot).capture();
+        Assertions.assertThrows(RuntimeException.class, () -> cmd.verify(TestConditions.throwsError()));
+    }
+    
+    @Test
+    public void multipleConditions_AllReturnTrue_ResultIsPass_AllAreAsserted() {
+        cmd.verify(TestConditions.returnsTrue(), TestConditions.returnsTrue(), TestConditions.returnsTrue());
+    }
+    
+    @Test
+    public void multipleConditions_OneReturnsFalse_ResultIsFail_AllAreAsserted() {
+        Mockito.doNothing().when(screenshot).capture();
+        Assertions.assertThrows(AssertionError.class, () ->
+                cmd.verify(TestConditions.returnsFalse(), TestConditions.returnsTrue(), TestConditions.returnsTrue()));
+    }
+    
+    @Test
+    public void multipleConditions_OneThrowsError_ResultIsError_AssertionStopsAtError() {
+        Mockito.doNothing().when(screenshot).capture();
+        Assertions.assertThrows(RuntimeException.class, () ->
+                cmd.verify(TestConditions.returnsTrue(), TestConditions.throwsError(), TestConditions.returnsTrue()));
+    }
+    
+    @Test
+    public void multipleConditions_MixFalseAndError_ResultIsError_AssertionStopsAtError() {
+        Mockito.doNothing().when(screenshot).capture();
+        Assertions.assertThrows(RuntimeException.class, () ->
+                cmd.verify(TestConditions.returnsFalse(), TestConditions.throwsError(), TestConditions.returnsTrue()));
+    }
+    
+    @Test
+    public void multipleConditions_MultipleThrowError_ResultIsError_AssertionStopsAtFirstError() {
+        Mockito.doNothing().when(screenshot).capture();
+        Assertions.assertThrows(RuntimeException.class, () ->
+                cmd.verify(TestConditions.returnsTrue(), TestConditions.throwsError(), TestConditions.throwsError()));
     }
 }
