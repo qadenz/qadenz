d092fbdf0d20167765d7e3dbad9daaafa274452f
Tim Slifer <tim-slifer@users.noreply.github.com>
2021-09-19 17:38:11 -0400

    Fixing something weird Intellij did to my static imports

--- src/test/java/io/qadenz/automation/test/VerifyTest.java
+++ src/test/java/io/qadenz/automation/test/VerifyTest.java
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -25,14 +25,14 @@ public class VerifyTest {
     
     @Test
     public void singleCondition_ReturnsTrue_ResultIsPass() {
-        Condition returnsTrue = Mockito.spy(TestConditions.returnsTrue());
+        Condition returnsTrue = spy(TestConditions.returnsTrue());
         commander.verify(returnsTrue);
         verify(returnsTrue, times(1)).result();
     }
     
     @Test
     public void singleCondition_ReturnsFalse_ResultIsFail() {
-        Condition returnsFalse = Mockito.spy(TestConditions.returnsFalse());
+        Condition returnsFalse = spy(TestConditions.returnsFalse());
         doNothing().when(screenshot).capture();
         assertThrows(AssertionError.class, () -> commander.verify(returnsFalse));
         verify(returnsFalse, times(1)).result();
@@ -40,7 +40,7 @@ public class VerifyTest {
     
     @Test
     public void singleCondition_ThrowsError_ResultIsError() {
-        Condition throwsError = Mockito.spy(TestConditions.throwsError());
+        Condition throwsError = spy(TestConditions.throwsError());
         doNothing().when(screenshot).capture();
         assertThrows(RuntimeException.class, () -> commander.verify(throwsError));
         verify(throwsError, times(1)).result();
@@ -48,9 +48,9 @@ public class VerifyTest {
     
     @Test
     public void multipleConditions_AllReturnTrue_ResultIsPass_AllAreAsserted() {
-        Condition returnsTrue1 = Mockito.spy(TestConditions.returnsTrue());
-        Condition returnsTrue2 = Mockito.spy(TestConditions.returnsTrue());
-        Condition returnsTrue3 = Mockito.spy(TestConditions.returnsTrue());
+        Condition returnsTrue1 = spy(TestConditions.returnsTrue());
+        Condition returnsTrue2 = spy(TestConditions.returnsTrue());
+        Condition returnsTrue3 = spy(TestConditions.returnsTrue());
         commander.verify(returnsTrue1, returnsTrue2, returnsTrue3);
         verify(returnsTrue1, times(1)).result();
         verify(returnsTrue2, times(1)).result();
@@ -59,9 +59,9 @@ public class VerifyTest {
     
     @Test
     public void multipleConditions_OneReturnsFalse_ResultIsFail_AllAreAsserted() {
-        Condition returnsFalse = Mockito.spy(TestConditions.returnsFalse());
-        Condition returnsTrue1 = Mockito.spy(TestConditions.returnsTrue());
-        Condition returnsTrue2 = Mockito.spy(TestConditions.returnsTrue());
+        Condition returnsFalse = spy(TestConditions.returnsFalse());
+        Condition returnsTrue1 = spy(TestConditions.returnsTrue());
+        Condition returnsTrue2 = spy(TestConditions.returnsTrue());
         doNothing().when(screenshot).capture();
         assertThrows(AssertionError.class, () -> commander.verify(returnsFalse, returnsTrue1, returnsTrue2));
         verify(returnsFalse, times(1)).result();
@@ -71,9 +71,9 @@ public class VerifyTest {
     
     @Test
     public void multipleConditions_OneThrowsError_ResultIsError_AssertionStopsAtError() {
-        Condition returnsTrue1 = Mockito.spy(TestConditions.returnsTrue());
-        Condition throwsError = Mockito.spy(TestConditions.throwsError());
-        Condition returnsTrue2 = Mockito.spy(TestConditions.returnsTrue());
+        Condition returnsTrue1 = spy(TestConditions.returnsTrue());
+        Condition throwsError = spy(TestConditions.throwsError());
+        Condition returnsTrue2 = spy(TestConditions.returnsTrue());
         doNothing().when(screenshot).capture();
         assertThrows(RuntimeException.class, () -> commander.verify(returnsTrue1, throwsError, returnsTrue2));
         verify(returnsTrue1, times(1)).result();
@@ -83,9 +83,9 @@ public class VerifyTest {
     
     @Test
     public void multipleConditions_MixFalseAndError_ResultIsError_AssertionStopsAtError() {
-        Condition returnsFalse = Mockito.spy(TestConditions.returnsFalse());
-        Condition throwsError = Mockito.spy(TestConditions.throwsError());
-        Condition returnsTrue = Mockito.spy(TestConditions.returnsTrue());
+        Condition returnsFalse = spy(TestConditions.returnsFalse());
+        Condition throwsError = spy(TestConditions.throwsError());
+        Condition returnsTrue = spy(TestConditions.returnsTrue());
         doNothing().when(screenshot).capture();
         assertThrows(RuntimeException.class, () -> commander.verify(returnsFalse, throwsError, returnsTrue));
         verify(returnsFalse, times(1)).result();
@@ -95,9 +95,9 @@ public class VerifyTest {
     
     @Test
     public void multipleConditions_MultipleThrowError_ResultIsError_AssertionStopsAtFirstError() {
-        Condition returnsTrue = Mockito.spy(TestConditions.returnsTrue());
-        Condition throwsError1 = Mockito.spy(TestConditions.throwsError());
-        Condition throwsError2 = Mockito.spy(TestConditions.throwsError());
+        Condition returnsTrue = spy(TestConditions.returnsTrue());
+        Condition throwsError1 = spy(TestConditions.throwsError());
+        Condition throwsError2 = spy(TestConditions.throwsError());
         doNothing().when(screenshot).capture();
         assertThrows(RuntimeException.class, () -> commander.verify(returnsTrue, throwsError1, throwsError2));
         verify(returnsTrue, times(1)).result();
