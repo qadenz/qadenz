6f063879298582076d967bb5caa7d99ed3208e18
Tim Slifer <tim-slifer@users.noreply.github.com>
2021-10-04 22:13:09 -0400

    Changing out MutableCapabilities for Capabilities
    ... for handling of browser options in the CapabilityProvider

--- src/main/java/io/qadenz/automation/config/CapabilityProvider.java
+++ src/main/java/io/qadenz/automation/config/CapabilityProvider.java
@@ -2,6 +2,7 @@ package io.qadenz.automation.config;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
@@ -30,7 +31,7 @@ public class CapabilityProvider {
      *
      * @return The Browser Options.
      */
-    public static MutableCapabilities getBrowserOptions() {
+    public static Capabilities getBrowserOptions() {
         Browser browser = WebConfig.browser;
         
         List<String> browserArgs = loadArgs(browser);
