8428b7c1890d6f3cad85e1e3fc25e71dc6b44309
Tim Slifer <tim-slifer@users.noreply.github.com>
2021-10-11 17:39:53 -0400

    Adding PolyForm license header to all source files

--- src/main/java/io/qadenz/automation/reporter/ScreenshotData.java
+++ src/main/java/io/qadenz/automation/reporter/ScreenshotData.java
@@ -1,3 +1,12 @@
+/*
+Copyright 2021 Tim Slifer
+
+Licensed under the PolyForm Internal Use License, Version 1.0.0 (the "License");
+you may not use this file except in compliance with the License.
+A copy of the License may be obtained at
+
+https://polyformproject.org/licenses/internal-use/1.0.0/
+ */
 package io.qadenz.automation.reporter;
 
 import java.util.HashMap;
