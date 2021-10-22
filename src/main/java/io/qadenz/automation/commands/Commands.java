10c3b99615f1b0d99a5cc980f2b3eab38ecb8e7c
Tim Slifer <tim@slifer.dev>
2021-10-22 12:01:00 -0400

    Cleaning up javadoc

--- src/main/java/io/qadenz/automation/commands/Commands.java
+++ src/main/java/io/qadenz/automation/commands/Commands.java
@@ -35,11 +35,12 @@ public abstract class Commands {
     }
     
     /**
-     * Evaluates each of the given conditions as a group. If one or more Conditions results in a failure, execution will
-     * not be aborted after the final Condition is evaluated. A call to {@link Assertions} will flush the failures and
-     * abort the test at a user designated point in the test. If a call to Verify exists after a call to Check and
-     * before a flush, the test will still be aborted if the verify produces a failure. Execution will be stopped
-     * immediately if an error is encountered.
+     * Evaluates each of the given {@link Condition}s as a group. Execution will be allowed to continue after the final
+     * Condition is evaluated, even if one or more Conditions results in a failure. A call to {@link Assertions} will
+     * flush the failures and abort the test at a user designated point in the test. If a call to {@code verify()}
+     * exists after a call to Check and before a call to {@code flush()}, the test will still be aborted if {@code
+     * verify()} produces a failure. Execution will be stopped immediately if an error is encountered during the {@code
+     * check()}.
      *
      * @param conditions The Conditions to be evaluated.
      */
@@ -66,9 +67,9 @@ public abstract class Commands {
     }
     
     /**
-     * Evaluates each of the given conditions as a group. If one or more Conditions results in a failure, execution will
-     * be aborted after the final Condition is evaluated. Execution will be stopped immediately if an error is
-     * encountered.
+     * Evaluates each of the given {@link Condition}s as a group. If one or more Conditions results in a failure,
+     * execution will be aborted after the final Condition is evaluated. Execution will be stopped immediately if an
+     * error is encountered.
      *
      * @param conditions The Conditions to be evaluated.
      */
