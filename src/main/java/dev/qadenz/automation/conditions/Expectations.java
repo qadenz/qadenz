3ea20ac28a03e64fe3e98cd5418d9e374cad59b2
Tim Slifer <tim-slifer@users.noreply.github.com>
2022-09-01 15:07:12 -0400

    Added date expectations

--- src/main/java/dev/qadenz/automation/conditions/Expectations.java
+++ src/main/java/dev/qadenz/automation/conditions/Expectations.java
@@ -9,11 +9,14 @@ https://polyformproject.org/licenses/internal-use/1.0.0/
  */
 package dev.qadenz.automation.conditions;
 
+import org.exparity.hamcrest.date.DateMatchers;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import static org.hamcrest.Matchers.emptyOrNullString;
@@ -59,6 +62,167 @@ public class Expectations {
             @Override
             public String description() {
                 return "is FALSE";
+            }
+        };
+    }
+    
+    /**
+     * An expectation for the text of an element, represented as a Date, to be after the given Date.
+     *
+     * @param date The formatted date value for comparison.
+     *
+     * @return The Expectation.
+     */
+    public static Expectation<Date> isAfter(final Date date) {
+        
+        return new Expectation<Date>() {
+            
+            @Override
+            public Matcher<Date> matcher() {
+                return DateMatchers.after(date);
+            }
+            
+            @Override
+            public String description() {
+                return "is after [" + date.toString() + "]";
+            }
+        };
+    }
+    
+    /**
+     * An expectation for the text of an element, represented as a Date, to be the same as or after the given Date.
+     *
+     * @param date The formatted date value for comparison.
+     *
+     * @return The Expectation.
+     */
+    public static Expectation<Date> isSameAsOrAfter(final Date date) {
+        
+        return new Expectation<Date>() {
+            
+            @Override
+            public Matcher<Date> matcher() {
+                return DateMatchers.sameOrAfter(date);
+            }
+            
+            @Override
+            public String description() {
+                return "is the same as or after [" + date.toString() + "]";
+            }
+        };
+    }
+    
+    /**
+     * An expectation for the text of an element, represented as a Date, to be the same as the given Date.
+     *
+     * @param date The formatted date value for comparison.
+     *
+     * @return The Expectation.
+     */
+    public static Expectation<Date> isSameAs(final Date date) {
+        
+        return new Expectation<Date>() {
+            
+            @Override
+            public Matcher<Date> matcher() {
+                return DateMatchers.sameDay(date);
+            }
+            
+            @Override
+            public String description() {
+                return "is the same as [" + date.toString() + "]";
+            }
+        };
+    }
+    
+    /**
+     * An expectation for the text of an element, represented as a Date, to be not the same as the given Date.
+     *
+     * @param date The formatted date value for comparison.
+     *
+     * @return The Expectation.
+     */
+    public static Expectation<Date> isNotSameAs(final Date date) {
+        
+        return new Expectation<Date>() {
+            
+            @Override
+            public Matcher<Date> matcher() {
+                return not(DateMatchers.sameDay(date));
+            }
+            
+            @Override
+            public String description() {
+                return "is the same as [" + date.toString() + "]";
+            }
+        };
+    }
+    
+    /**
+     * An expectation for the text of an element, represented as a Date, to be the same as or before the given Date.
+     *
+     * @param date The formatted date value for comparison.
+     *
+     * @return The Expectation.
+     */
+    public static Expectation<Date> isSameAsOrBefore(final Date date) {
+        
+        return new Expectation<Date>() {
+            
+            @Override
+            public Matcher<Date> matcher() {
+                return DateMatchers.sameOrBefore(date);
+            }
+            
+            @Override
+            public String description() {
+                return "is the same as or before [" + date.toString() + "]";
+            }
+        };
+    }
+    
+    /**
+     * An expectation for the text of an element, represented as a Date, to be within a timeframe of the given Date.
+     *
+     * @param date The formatted date value for comparison.
+     *
+     * @return The Expectation.
+     */
+    public static Expectation<Date> isWithin(final Long period, final ChronoUnit unit, final Date date) {
+        
+        return new Expectation<Date>() {
+            
+            @Override
+            public Matcher<Date> matcher() {
+                return DateMatchers.within(period, unit, date);
+            }
+            
+            @Override
+            public String description() {
+                return "is within [" + period + " " + unit.toString() + "] of [" + date.toString() + "]";
+            }
+        };
+    }
+    
+    /**
+     * An expectation for the text of an element, represented as a Date, to be before the given Date.
+     *
+     * @param date The formatted date value for comparison.
+     *
+     * @return The Expectation.
+     */
+    public static Expectation<Date> isBefore(final Date date) {
+        
+        return new Expectation<Date>() {
+            
+            @Override
+            public Matcher<Date> matcher() {
+                return DateMatchers.before(date);
+            }
+            
+            @Override
+            public String description() {
+                return "is before [" + date.toString() + "]";
             }
         };
     }
