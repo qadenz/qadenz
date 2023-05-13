e94f215b941939b5dd8fabfb60a2b6b615fa13e6
Tim Slifer <tim-slifer@users.noreply.github.com>
2023-05-13 02:05:22 -0400

    Repackaged validator unit tests

--- src/test/java/dev/qadenz/automation/test/validators/VerifyTest.java
+++ src/test/java/dev/qadenz/automation/test/validators/VerifyTest.java
@@ -7,7 +7,7 @@ A copy of the License may be obtained at
 
 https://polyformproject.org/licenses/internal-use/1.0.0/
  */
-package dev.qadenz.automation.test;
+package dev.qadenz.automation.test.validators;
 
 import dev.qadenz.automation.reporter.Screenshot;
 import dev.qadenz.automation.conditions.Condition;
