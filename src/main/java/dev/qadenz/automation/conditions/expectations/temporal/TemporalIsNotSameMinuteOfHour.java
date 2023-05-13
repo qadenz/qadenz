1156dfd7645e9a8425aef1b8e5d4936cc28f4d17
Tim Slifer <tim-slifer@users.noreply.github.com>
2023-05-13 01:06:29 -0400

    Added some missing TemporalExpectation impls

---
+++ src/main/java/dev/qadenz/automation/conditions/expectations/temporal/TemporalIsNotSameMinuteOfHour.java
@@ -0,0 +1,58 @@
+/*
+Copyright 2021 Tim Slifer
+
+Licensed under the PolyForm Internal Use License, Version 1.0.0 (the "License");
+you may not use this file except in compliance with the License.
+A copy of the License may be obtained at
+
+https://polyformproject.org/licenses/internal-use/1.0.0/
+ */
+package dev.qadenz.automation.conditions.expectations.temporal;
+
+import dev.qadenz.automation.conditions.TemporalExpectation;
+import org.exparity.hamcrest.date.core.IsMinute;
+import org.exparity.hamcrest.date.core.TemporalConverter;
+import org.exparity.hamcrest.date.core.TemporalProvider;
+import org.exparity.hamcrest.date.core.types.Minute;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.Temporal;
+
+/**
+ * An expectation for the text of an element, represented as a Temporal, to be not the same minute as the given
+ * Temporal.
+ *
+ * @author Tim Slifer
+ */
+public class TemporalIsNotSameMinuteOfHour<T> implements TemporalExpectation<T> {
+    
+    private Temporal temporal;
+    private TemporalConverter<T, Minute> converter;
+    private TemporalProvider<Minute> provider;
+    
+    private DateTimeFormatter dateTimeFormatter;
+    
+    public TemporalIsNotSameMinuteOfHour(Temporal temporal, TemporalConverter<T, Minute> converter,
+            TemporalProvider<Minute> provider) {
+        this.temporal = temporal;
+        this.converter = converter;
+        this.provider = provider;
+    }
+    
+    @Override
+    public Matcher<T> matcher() {
+        return Matchers.not(new IsMinute<>(converter, provider));
+    }
+    
+    @Override
+    public String description() {
+        return "is not same minute as [" + dateTimeFormatter.format(temporal) + "]";
+    }
+    
+    @Override
+    public void setDateTimeFormatter(DateTimeFormatter dateTimeFormatter) {
+        this.dateTimeFormatter = dateTimeFormatter;
+    }
+}
