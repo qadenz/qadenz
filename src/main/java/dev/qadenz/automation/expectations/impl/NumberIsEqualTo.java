456d005130c3240f035a277f7a402c585c5c0cd7
Tim Slifer <tim-slifer@users.noreply.github.com>
2023-06-04 23:23:56 -0400

    Repackaged Expectations implementations

---
+++ src/main/java/dev/qadenz/automation/expectations/impl/NumberIsEqualTo.java
@@ -0,0 +1,47 @@
+/*
+Copyright 2021 Tim Slifer
+
+Licensed under the PolyForm Internal Use License, Version 1.0.0 (the "License");
+you may not use this file except in compliance with the License.
+A copy of the License may be obtained at
+
+https://polyformproject.org/licenses/internal-use/1.0.0/
+ */
+package dev.qadenz.automation.expectations.impl;
+
+import dev.qadenz.automation.expectations.NumericExpectation;
+import org.hamcrest.Matcher;
+import org.hamcrest.core.IsEqual;
+
+import java.text.NumberFormat;
+
+/**
+ * An Expectation for a number value to be equal to the given value.
+ *
+ * @author Tim Slifer
+ */
+public class NumberIsEqualTo<T> implements NumericExpectation<T> {
+    
+    private T expectedValue;
+    
+    private NumberFormat numberFormat;
+    
+    public NumberIsEqualTo(T expectedValue) {
+        this.expectedValue = expectedValue;
+    }
+    
+    @Override
+    public Matcher<T> matcher() {
+        return new IsEqual<>(expectedValue);
+    }
+    
+    @Override
+    public String description() {
+        return "is equal to [" + numberFormat.format(expectedValue) + "]";
+    }
+    
+    @Override
+    public void setNumberFormat(NumberFormat numberFormat) {
+        this.numberFormat = numberFormat;
+    }
+}
---
+++ src/main/java/dev/qadenz/automation/expectations/impl/TextContainsIgnoreCase.java
@@ -0,0 +1,38 @@
+/*
+Copyright 2021 Tim Slifer
+
+Licensed under the PolyForm Internal Use License, Version 1.0.0 (the "License");
+you may not use this file except in compliance with the License.
+A copy of the License may be obtained at
+
+https://polyformproject.org/licenses/internal-use/1.0.0/
+ */
+package dev.qadenz.automation.expectations.impl;
+
+import dev.qadenz.automation.expectations.Expectation;
+import org.hamcrest.Matcher;
+import org.hamcrest.core.StringContains;
+
+/**
+ * An Expectation for a String value to contain the given value, ignoring case.
+ *
+ * @author Tim Slifer
+ */
+public class TextContainsIgnoreCase implements Expectation<String> {
+    
+    private String expectedText;
+    
+    public TextContainsIgnoreCase(String expectedText) {
+        this.expectedText = expectedText;
+    }
+    
+    @Override
+    public Matcher<String> matcher() {
+        return new StringContains(true, expectedText);
+    }
+    
+    @Override
+    public String description() {
+        return "ignoring case, contains [" + expectedText + "]";
+    }
+}
