90bea8c99a9c5621d1164fae86d29452d3d77a48
Tim Slifer <tim-slifer@users.noreply.github.com>
2023-06-06 21:44:52 -0400

    Reorganized and filled gaps on Integer expectations tests

--- src/test/java/dev/qadenz/automation/test/expectations/NumericExpectationIntegerTest.java
+++ src/test/java/dev/qadenz/automation/test/expectations/NumericExpectationIntegerTest.java
@@ -9,8 +9,8 @@ https://polyformproject.org/licenses/internal-use/1.0.0/
  */
 package dev.qadenz.automation.test.expectations;
 
-import dev.qadenz.automation.expectations.Expectations;
 import dev.qadenz.automation.conditions.NumberFormatters;
+import dev.qadenz.automation.expectations.Expectations;
 import dev.qadenz.automation.expectations.NumericExpectation;
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +20,20 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class NumericExpectationIntegerTest {
     
     @Test
+    public void testIsEqualTo_ReturnsFalseWhenActualIsGreaterThanExpected() {
+        NumericExpectation<Integer> expectation = Expectations.isEqualTo(1000);
+        expectation.setNumberFormat(NumberFormatters.nonGroupedNumber());
+        assertFalse(expectation.matcher().matches(1100));
+    }
+    
+    @Test
+    public void testIsEqualTo_ReturnsFalseWhenActualIsLessThanExpected() {
+        NumericExpectation<Integer> expectation = Expectations.isEqualTo(1000);
+        expectation.setNumberFormat(NumberFormatters.nonGroupedNumber());
+        assertFalse(expectation.matcher().matches(900));
+    }
+    
+    @Test
     public void testIsEqualTo_ReturnsTrueWhenActualIsEqualToExpected() {
         NumericExpectation<Integer> expectation = Expectations.isEqualTo(1000);
         expectation.setNumberFormat(NumberFormatters.nonGroupedNumber());
@@ -27,24 +41,10 @@ public class NumericExpectationIntegerTest {
     }
     
     @Test
-    public void testIsEqualTo_ReturnsFalseWhenActualIsNotEqualToExpected() {
-        NumericExpectation<Integer> expectation = Expectations.isEqualTo(1000);
-        expectation.setNumberFormat(NumberFormatters.nonGroupedNumber());
-        assertFalse(expectation.matcher().matches(1100));
-    }
-    
-    @Test
-    public void testIsGreaterThan_ReturnsTrueWhenActualIsGreaterThenExpected() {
+    public void testIsGreaterThan_ReturnsTrueWhenActualIsGreaterThanExpected() {
         NumericExpectation<Integer> expectation = Expectations.isGreaterThan(1000);
         expectation.setNumberFormat(NumberFormatters.nonGroupedNumber());
         assertTrue(expectation.matcher().matches(1100));
-    }
-    
-    @Test
-    public void testIsGreaterThan_ReturnsFalseWhenActualIsEqualToExpected() {
-        NumericExpectation<Integer> expectation = Expectations.isGreaterThan(1000);
-        expectation.setNumberFormat(NumberFormatters.nonGroupedNumber());
-        assertFalse(expectation.matcher().matches(1000));
     }
     
     @Test
@@ -55,17 +55,17 @@ public class NumericExpectationIntegerTest {
     }
     
     @Test
-    public void testIsGreaterThanOrEqualTo_ReturnsTrueWhenActualIsGreaterThenExpected() {
-        NumericExpectation<Integer> expectation = Expectations.isGreaterThanOrEqualTo(1000);
+    public void testIsGreaterThan_ReturnsFalseWhenActualIsEqualToExpected() {
+        NumericExpectation<Integer> expectation = Expectations.isGreaterThan(1000);
         expectation.setNumberFormat(NumberFormatters.nonGroupedNumber());
-        assertTrue(expectation.matcher().matches(1100));
+        assertFalse(expectation.matcher().matches(1000));
     }
     
     @Test
-    public void testIsGreaterThanOrEqualTo_ReturnsTrueWhenActualIsEqualToExpected() {
+    public void testIsGreaterThanOrEqualTo_ReturnsTrueWhenActualIsGreaterThanExpected() {
         NumericExpectation<Integer> expectation = Expectations.isGreaterThanOrEqualTo(1000);
         expectation.setNumberFormat(NumberFormatters.nonGroupedNumber());
-        assertTrue(expectation.matcher().matches(1000));
+        assertTrue(expectation.matcher().matches(1100));
     }
     
     @Test
@@ -76,7 +76,21 @@ public class NumericExpectationIntegerTest {
     }
     
     @Test
-    public void testIsLessThan_ReturnsTrueWhenActualIsLessThenExpected() {
+    public void testIsGreaterThanOrEqualTo_ReturnsTrueWhenActualIsEqualToExpected() {
+        NumericExpectation<Integer> expectation = Expectations.isGreaterThanOrEqualTo(1000);
+        expectation.setNumberFormat(NumberFormatters.nonGroupedNumber());
+        assertTrue(expectation.matcher().matches(1000));
+    }
+    
+    @Test
+    public void testIsLessThan_ReturnsFalseWhenActualIsGreaterThanExpected() {
+        NumericExpectation<Integer> expectation = Expectations.isLessThan(1000);
+        expectation.setNumberFormat(NumberFormatters.nonGroupedNumber());
+        assertFalse(expectation.matcher().matches(1100));
+    }
+    
+    @Test
+    public void testIsLessThan_ReturnsTrueWhenActualIsLessThanExpected() {
         NumericExpectation<Integer> expectation = Expectations.isLessThan(1000);
         expectation.setNumberFormat(NumberFormatters.nonGroupedNumber());
         assertTrue(expectation.matcher().matches(900));
@@ -90,14 +104,14 @@ public class NumericExpectationIntegerTest {
     }
     
     @Test
-    public void testIsLessThan_ReturnsFalseWhenActualIsGreaterThanExpected() {
-        NumericExpectation<Integer> expectation = Expectations.isLessThan(1000);
+    public void testIsLessThanOrEqualTo_ReturnsFalseWhenActualIsGreaterThanExpected() {
+        NumericExpectation<Integer> expectation = Expectations.isLessThanOrEqualTo(1000);
         expectation.setNumberFormat(NumberFormatters.nonGroupedNumber());
         assertFalse(expectation.matcher().matches(1100));
     }
     
     @Test
-    public void testIsLessThanOrEqualTo_ReturnsTrueWhenActualIsLessThenExpected() {
+    public void testIsLessThanOrEqualTo_ReturnsTrueWhenActualIsLessThanExpected() {
         NumericExpectation<Integer> expectation = Expectations.isLessThanOrEqualTo(1000);
         expectation.setNumberFormat(NumberFormatters.nonGroupedNumber());
         assertTrue(expectation.matcher().matches(900));
@@ -111,17 +125,17 @@ public class NumericExpectationIntegerTest {
     }
     
     @Test
-    public void testIsLessThanOrEqualTo_ReturnsFalseWhenActualIsGreaterThanExpected() {
-        NumericExpectation<Integer> expectation = Expectations.isLessThanOrEqualTo(1000);
-        expectation.setNumberFormat(NumberFormatters.nonGroupedNumber());
-        assertFalse(expectation.matcher().matches(1100));
-    }
-    
-    @Test
-    public void testIsNotEqualTo_ReturnsTrueWhenActualIsNotEqualToExpected() {
+    public void testIsNotEqualTo_ReturnsTrueWhenActualIsGreaterThanExpected() {
         NumericExpectation<Integer> expectation = Expectations.isNotEqualTo(1000);
         expectation.setNumberFormat(NumberFormatters.nonGroupedNumber());
         assertTrue(expectation.matcher().matches(1100));
+    }
+    
+    @Test
+    public void testIsNotEqualTo_ReturnsTrueWhenActualIsLessThanExpected() {
+        NumericExpectation<Integer> expectation = Expectations.isNotEqualTo(1000);
+        expectation.setNumberFormat(NumberFormatters.nonGroupedNumber());
+        assertTrue(expectation.matcher().matches(900));
     }
     
     @Test
