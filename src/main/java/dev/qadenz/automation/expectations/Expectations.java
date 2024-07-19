9ef65431980200c0207329130180ab8d1ae98fce
Tim Slifer <tim-slifer@users.noreply.github.com>
2024-07-19 01:20:53 -0400

    Removed unneeded list components

--- src/main/java/dev/qadenz/automation/expectations/Expectations.java
+++ src/main/java/dev/qadenz/automation/expectations/Expectations.java
@@ -11,7 +11,6 @@ package dev.qadenz.automation.expectations;
 
 import dev.qadenz.automation.expectations.impl.IsFalse;
 import dev.qadenz.automation.expectations.impl.IsTrue;
-import dev.qadenz.automation.expectations.impl.ListInOrder;
 import dev.qadenz.automation.expectations.impl.NumberIsEqualTo;
 import dev.qadenz.automation.expectations.impl.NumberIsGreaterThan;
 import dev.qadenz.automation.expectations.impl.NumberIsGreaterThanOrEqualTo;
@@ -47,7 +46,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
-import java.util.List;
 
 import static org.exparity.hamcrest.date.core.TemporalConverters.LOCALDATETIME_AS_DAYOFWEEK;
 import static org.exparity.hamcrest.date.core.TemporalConverters.LOCALDATETIME_AS_LOCALDATETIME;
@@ -720,17 +718,5 @@ public class Expectations {
      */
     public static Expectation<String> startsWith(String expectedText) {
         return new TextStartsWith(expectedText);
-    }
-    
-    /**
-     * An Expectation for a List of String 'actual' values to be equal to the given List of 'expected' values, and in
-     * the same order.
-     *
-     * @param expectedValues The List of values for comparison.
-     *
-     * @return The Expectation.
-     */
-    public static Expectation<Iterable<?>> listInOrder(List<String> expectedValues) {
-        return new ListInOrder(expectedValues);
     }
 }
