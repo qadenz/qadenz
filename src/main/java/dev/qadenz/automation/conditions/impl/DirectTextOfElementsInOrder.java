eee13a2bc8667fdb94f5b41c1e91a337f62c3af9
Tim Slifer <tim-slifer@users.noreply.github.com>
2024-07-19 21:59:46 -0400

    Added direct text of elements condition

---
+++ src/main/java/dev/qadenz/automation/conditions/impl/DirectTextOfElementsInOrder.java
@@ -0,0 +1,55 @@
+package dev.qadenz.automation.conditions.impl;
+
+import dev.qadenz.automation.commands.WebInspector;
+import dev.qadenz.automation.conditions.Condition;
+import dev.qadenz.automation.conditions.Conditions;
+import dev.qadenz.automation.conditions.OrderedListComparator;
+import dev.qadenz.automation.expectations.ListExpectation;
+import dev.qadenz.automation.ui.Locator;
+
+import java.util.List;
+
+/**
+ * A Condition to evaluate the visible inner text of each instance of an element, excluding the text of any descendant
+ * elements on the DOM, as an ordered list.
+ *
+ * @author Tim Slifer
+ */
+public class DirectTextOfElementsInOrder implements Condition {
+    
+    private Locator locator;
+    private ListExpectation expectation;
+    
+    private String failures;
+    
+    public DirectTextOfElementsInOrder(Locator locator, ListExpectation expectation) {
+        this.locator = locator;
+        this.expectation = expectation;
+    }
+    
+    @Override
+    public Boolean result() {
+        Boolean match = null;
+        WebInspector webInspector = new WebInspector(Conditions.class);
+        List<String> elementValues = webInspector.getDirectTextOfElements(locator);
+        
+        OrderedListComparator orderedListComparator = new OrderedListComparator(expectation, elementValues);
+        match = orderedListComparator.getResult();
+        
+        if (!match) {
+            failures = orderedListComparator.getFailures();
+        }
+        
+        return match;
+    }
+    
+    @Override
+    public String actual() {
+        return "Discrepancies: \n" + failures;
+    }
+    
+    @Override
+    public String toString() {
+        return "Direct text of each instance of element [" + locator.getName() + "] " + expectation + ".";
+    }
+}
